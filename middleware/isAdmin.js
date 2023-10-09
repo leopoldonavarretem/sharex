2cd83bdb5b689b0ae0db5c2cc1978fe9cfc686f9
Leopoldo Navarrete <leopoldonm@outlook.com>
2023-10-09 22:59:01 +0000

    Fix: Middleware redirects correctly

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) =>{
     // Checks if the user is an admin
-    if(req.session.user.admin) {
-        return res.redirect('/auth/login');
+    if(!req.session.user.admin) {
+        return res.redirect('/notAuthorized');
     }
     next();
 }
