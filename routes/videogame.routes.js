e0ef0fb3a628e4b38250479e6c71d3d14b1fdf24
Leopoldo Navarrete <leopoldonm@outlook.com>
2023-10-11 20:31:15 +0000

    Feat: Videogame route complete

--- routes/videogame.routes.js
+++ routes/videogame.routes.js
@@ -1,36 +1,46 @@
 // Imports
 const router = require("express").Router();
+
+// Import Models
 const Videogame = require('../models/Videogame.model');
+const Review = require('../models/Review.model');
 
-// Middleware Imports
-const isLoggedIn = require('../middleware/isLoggedIn');
-
-// This route will show all the videogames
-router.get('/', (req, res)=>{
-  Videogame.find()
-    .then((videogamesData)=>{
-        res.render('media/videogames', {videogamesData}) 
-    })
-    .catch(console.log)
+// Show all videogames 
+router.get('/', async(req, res, next)=>{
+  // Get necessary data
+  const userData = req.session.user
+  try{
+    const videogamesData = await Videogame.find()
+    return res.render('media/videogames', {videogamesData, userData}) 
+  }
+  catch(err){
+   return next(500) 
+  }
 });
 
 //This route will show you a single videogame
-router.get('/:videogame', isLoggedIn, (req, res)=>{
+router.get('/:videogame', async (req, res, next)=> {
 
-})
-// This route will create a new videogame
-router.post('/:videogame', isLoggedIn, (req,res)=>{
+  // Get required data
+  const userData = req.session.user
+  const errorMessage = req.session.reviewErrors
+ 
+  if(errorMessage) delete req.session.reviewErrors
 
-})
-
-// This route will update the videogame
-router.patch('/:videogame', isLoggedIn, (req, res)=>{
-
-})
-
-//This route will delete the videogame
-router.delete('/:videogame', isLoggedIn, (req, res)=>{
-
+  try{
+    const videogameData = await Videogame.findById(req.params.videogame)
+    
+    const reviewsData = await Review.find({externalId: videogameData._id})
+    .populate('userId', 'username')
+    .catch(()=>{
+      return []
+    });
+    
+    return res.render('media/videogame', {videogameData, userData, errorMessage, reviewsData})
+  }
+  catch(err){
+    next(500)
+  }
 })
 
 module.exports = router;
