53a90b26263852d0dede8d801a102a9daf30a2af
Leopoldo Navarrete <leopoldonm@outlook.com>
2023-10-13 03:16:31 +0000

    Feat: Admins can create new media

--- models/Media.model.js
+++ models/Media.model.js
@@ -5,37 +5,42 @@ const mediaSchema = new Schema(
   {
     mediaName: {
       type: String,
-      required: true,
+      required: [true, 'Media name is required']
     },
 
     description: {
       type: String,
-      required: true,
+      required: [true, 'Description is required']
     },
 
     genre: {
       type: String,
-      required: true,
+      required: [true, 'Genre is required.']
     },
 
     year: {
       type: Number,
-      required: true,
+      required: [true, 'Year is required.'],
+      min: 0,
+      max: 2050
     },
 
     creator: {
       type: String,
-      required: true,
+      required: [true, 'Creator is required.']
     },
 
     imageUrl: {
-      type: String
+      type: String,
+      required: [true, 'Image is required.']
     },
 
     mediaType:{
-      type: String
-    }
-  },
+      type: String,
+      enum: ['Book', 'Videogame', 'Serie', 'Movie', 'Album'],
+      required: [true, 'Media type is required.']
+    },
+ },
 
   {
     timestamps: true,
---
+++ routes/admin.routes.js
@@ -0,0 +1,80 @@
+// Imports
+const router = require('express').Router();
+const uploadFile = require("../config/cloudinary.config");
+
+// Import Models
+const Media = require('../models/Media.model');
+
+// Middleware Imports
+const isLoggedIn = require('../middleware/isLoggedIn');
+const isAdmin = require('../middleware/isAdmin');
+
+// Access to admin page
+router.get('/', isLoggedIn, isAdmin, (req,res)=>{
+
+  // Get the required data
+  const userData = req.session.user;
+  const errorMessage = req.session.mediaErrors;
+
+  if(errorMessage) delete req.session.mediaErrors;
+
+  res.render('admin/console', {userData, errorMessage});
+})
+
+// Create media
+router.post('/', isLoggedIn, isAdmin, uploadFile.single('imageUrl'), async (req, res, next)=>{
+
+  // Destructuring the request
+  let {mediaName, description, genre, year, creator, mediaType} = req.body;
+  const { path } = req.file ? req.file : [];
+  
+  // Transform the data
+  year = Number(year);
+
+  // Validate data
+  req.session.mediaErrors = [];
+  
+  // Check that the informaton is valid
+  if (typeof mediaName !== 'string') req.session.mediaErrors.push('Media Name must be of type string');
+  if (typeof description !== 'string') req.session.mediaErrors.push('Description must be of type string');
+  if (typeof genre !== 'string') req.session.mediaErrors.push('Genre can only be of type string');
+  if (typeof year !== 'number' || year < 0 || year > 2050 ) req.session.mediaErrors.push('Input a valid year.');
+  if (typeof creator !== 'string') req.session.mediaErrors.push('Creator must be of type string.');
+  if (typeof path !== 'string') req.session.mediaErrors.push('Image must be uploaded');
+  if(!['Book', 'Videogame', 'Movie', 'Serie', 'Album'].includes(mediaType)) req.session.mediaErrors.push('Input a valid Media Type');
+  
+  // Send the user back to the request if there are any errors
+  if (req.session.mediaErrors.length > 0)  return res.redirect(`/admin`);
+  
+  // Create videogame object
+  const newMedia = {mediaName, description, genre, year, creator, mediaType, imageUrl: path};
+
+  try{
+    await Media.create(newMedia);
+  
+    return res.redirect('/admin');
+  }
+  catch(err){
+    next(500);
+  };
+});
+
+//This route will delete the videogame
+router.get('/:mediaId', isLoggedIn, isAdmin, async (req, res, next)=>{
+  
+  // Validate the ObjectId
+  if (!isValidObjectId(req.params.mediaId)) return next(400);
+
+  try{
+      
+    // Delete the Review
+    await Media.findByIdAndDelete(req.params.mediaId);
+
+    return res.redirect('/admin');
+  }
+  catch(err){
+    return next(500);
+  };
+});
+
+module.exports = router;
