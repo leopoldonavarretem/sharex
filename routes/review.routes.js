d193d4ae2bcd872f9f030482e75c003f081194ab
Leopoldo Navarrete <leopoldonm@outlook.com>
2023-10-13 03:18:16 +0000

    Fix: Sort is now implemented

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -15,7 +15,7 @@ const isValidObjectId = (id) => ObjectId.isValid(id) && (new Object(id)).toStrin
 router.post('/', isLoggedIn, async (req,res, next)=>{
   
   // Destructuring the request
-  let {review, rating, mediaId, like, mediaType} = req.body 
+  let {review, rating, mediaId, like, mediaType} = req.body; 
   const userId = req.session.user._id;
   
   // Transform data
@@ -23,11 +23,11 @@ router.post('/', isLoggedIn, async (req,res, next)=>{
   rating = Number(rating);
 
   // Validate data
-  req.session.reviewErrors = [] 
+  req.session.reviewErrors = [];
 
   //Check that it is a valid external Id
-  if (!isValidObjectId(mediaId)) return next(400) 
-  if (!['Videogame', 'Movie', 'Album', 'serie', 'Book'].includes(mediaType)) return next(400)
+  if (!isValidObjectId(mediaId)) return next(400);
+  if (!['Videogame', 'Movie', 'Album', 'serie', 'Book'].includes(mediaType)) return next(400);
 
   // Check that the review object is valid
   if (typeof review !== 'string' || review.length < 30) req.session.reviewErrors.push("Review must only include text and a minimum length of 30");
@@ -35,19 +35,20 @@ router.post('/', isLoggedIn, async (req,res, next)=>{
   if (typeof like !== 'boolean') req.session.reviewErrors.push('Like can only be true or false');
 
   // Send the user back to the request if there are any errors
-  if (req.session.reviewErrors.length > 0) return res.redirect(`/media/${mediaId}`)
+  if (req.session.reviewErrors.length > 0) return res.redirect(`/media/${mediaId}`);
   
   // Create review object
-  const newReview = {review, rating, mediaId, like, userId}
+  const newReview = {review, rating, mediaId, like, userId};
 
   try{    
     // Create a new Review
-    await Review.create(newReview)
-    return res.redirect(`/media/${mediaId}`)
+    await Review.create(newReview);
+    
+    return res.redirect(`/media/${mediaId}`);
   }
   catch(err){
-    return next(500)
-  }
+    return next(500);
+  };
 });
 
 //This route will delete the movie 
@@ -60,19 +61,20 @@ router.get('/:reviewId', isLoggedIn, async(req, res, next)=>{
 
   try{
     // Request the review
-    const review = await Review.findById(req.params.reviewId)
-      .populate('userId', 'username')
+    const review = await Review.findById(req.params.reviewId).sort({createdAt : -1})
+      .populate('userId', 'username');
 
-      // Validate that the owner is deleting it
-      if (review.userId.username !== user.username) return next(401) 
+    // Validate that the owner is deleting it
+    if (review.userId.username !== user.username) return next(401) 
       
-      // Delete the Review
-      await Review.findByIdAndDelete(req.params.reviewId)
-      return res.redirect(`/users/${user._id}`)
+    // Delete the Review
+    await Review.findByIdAndDelete(req.params.reviewId)
+    
+    return res.redirect(`/users/${user._id}`)
   }
   catch(err){
     return next(500)
-  }
+  };
 });
 
 module.exports = router;
