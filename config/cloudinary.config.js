1098957da8aecb9845ab8e24f2859aff21f7c460
Leopoldo Navarrete <leopoldonm@outlook.com>
2023-10-13 03:16:31 +0000

    Feat: Admins can create new media

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -11,12 +11,12 @@ cloudinary.config({
  
   
 const storage = new CloudinaryStorage({
-  // cloudinary: cloudinary,
   cloudinary,
-  params: {
-    allowed_formats: ['jpg', 'png'],
-    folder: 'sharex' // The name of the folder in cloudinary
-    // resource_type: 'raw' => this is in case you want to upload other type of files, not just images
+  params: async(req, file) =>{
+    return{
+      format: 'jpeg',
+      folder: 'sharex'
+    }
   }
 });
  
