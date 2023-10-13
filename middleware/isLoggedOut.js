65f06f23caf13aac941918b02f6954529551c931
Leopoldo Navarrete <leopoldonm@outlook.com>
2023-10-13 03:15:31 +0000

    Style: Code clean up

--- middleware/isLoggedOut.js
+++ middleware/isLoggedOut.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
-    // if an already logged in user tries to access the login page it
-    // redirects the user to the home page
+    // Checks if the user is logged in
     if (req.session.user) {
       return res.redirect('/');
     }
